1b2d891b56e2a8c1fa4b704d49cc351ef1f7c101
Kevin Umayam <kevin.m.umayam@gmail.com>
2024-02-06 20:52:53 -0800

    added the cicular que data structure

--- linkedLists/app.js
+++ linkedLists/app.js
@@ -1,7 +1,17 @@
-const LinkedList = require('./linkedLists')
 
-const ll = LinkedList.fromValues(10, 20, 30, 40);
-ll.print()
-console.log(ll.getByIndex(2).value);
-ll.removeHead()
-ll.print()
+// !! linked listHere
+// const LinkedList = require('./linkedLists')
+
+// const ll = LinkedList.fromValues(10, 20, 30, 40);
+// ll.print()
+// console.log(ll.getByIndex(2).value);
+// ll.removeHead()
+// ll.print()
+
+
+// !! linked listHere
+
+const CircularQueue = require('./circularQueue')
+
+const queu = new CircularQueue(5);
+console.log(queu.isEmpty());
